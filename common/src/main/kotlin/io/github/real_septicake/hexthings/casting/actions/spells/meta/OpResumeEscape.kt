6a2b0c9e77fe5c3ced3707eebf22a5a831230ad8
Septicake <septicake056@gmail.com>
2025-11-13 19:33:46 -0500

    Retrojection no longer automatically places Introspection and Retrospection around the contents of the previous stack, added examples to Patience, did other cool things too probably

--- common/src/main/kotlin/io/github/real_septicake/hexthings/casting/actions/spells/meta/OpResumeEscape.kt
+++ common/src/main/kotlin/io/github/real_septicake/hexthings/casting/actions/spells/meta/OpResumeEscape.kt
@@ -3,10 +3,8 @@ package io.github.real_septicake.hexthings.casting.actions.spells.meta
 import at.petrak.hexcasting.api.casting.castables.Action
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.OperationResult
-import at.petrak.hexcasting.api.casting.eval.SpecialPatterns
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
-import at.petrak.hexcasting.api.casting.iota.PatternIota
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 import io.github.real_septicake.hexthings.casting.mishaps.MishapInvalidUnquote
 
@@ -22,13 +20,13 @@ object OpResumeEscape : Action {
             throw MishapInvalidUnquote()
         val prevImage = CastingImage.loadFromNbt(prev, env.world)
         val newParens = prevImage.parenthesized.toMutableList()
-        newParens.add(
-            CastingImage.ParenthesizedIota(PatternIota(SpecialPatterns.INTROSPECTION), false)
-        )
+//        newParens.add(
+//            CastingImage.ParenthesizedIota(PatternIota(SpecialPatterns.INTROSPECTION), false)
+//        )
         newParens.addAll(stack.map { CastingImage.ParenthesizedIota(it, false) })
-        newParens.add(
-            CastingImage.ParenthesizedIota(PatternIota(SpecialPatterns.RETROSPECTION), false)
-        )
+//        newParens.add(
+//            CastingImage.ParenthesizedIota(PatternIota(SpecialPatterns.RETROSPECTION), false)
+//        )
         val newPrevImage = prevImage.copy(parenthesized = newParens, opsConsumed = prevImage.opsConsumed + image.opsConsumed)
         return OperationResult(newPrevImage, listOf(), continuation, HexEvalSounds.THOTH)
     }
